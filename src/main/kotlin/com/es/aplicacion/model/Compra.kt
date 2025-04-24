93d653763618a6dcb54546378b8f345ddd4052f0
fbatlos <bdlsfrancisco@iesrafaelalberti.es>
2025-04-24 14:25:56 +0200

    Compra añadida, falta controller y service de valoracion y compra

---
+++ src/main/kotlin/com/es/aplicacion/model/Compra.kt
@@ -0,0 +1,26 @@
+﻿package com.es.aplicacion.model
+
+import org.springframework.data.mongodb.core.mapping.Document
+
+@Document("Compras")
+data class Compra(
+    val usuarioId: String,
+    val items: List<CompraItem>,
+    val metodoDePago: MetodoDePago
+)
+
+
+data class CompraItem(
+    val libroId: String,
+    val precio: Int,
+    val type: TipoStock
+)
+
+
+data class MetodoDePago(
+    val token: String,
+    val tipo: TipoDePago
+)
+enum class TipoDePago {
+    STRIPE_CARD, GOOGLE_PAY, APPLE_PAY
+}
