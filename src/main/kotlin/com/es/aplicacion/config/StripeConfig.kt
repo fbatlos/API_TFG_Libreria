efcb8f09a23497fe92fd1c451a6d3cbd5e5c856f
fbatlos <bdlsfrancisco@iesrafaelalberti.es>
2025-04-27 18:27:14 +0200

    Añadido todo de compras y valoracion, solo flata probarlo a fondo

--- src/main/kotlin/com/es/aplicacion/config/StripeConfig.kt
+++ src/main/kotlin/com/es/aplicacion/config/StripeConfig.kt
@@ -3,15 +3,25 @@
 import com.stripe.Stripe
 import jakarta.annotation.PostConstruct
 import org.springframework.beans.factory.annotation.Value
+import org.springframework.boot.context.properties.ConfigurationProperties
+import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 
 @Configuration
-class StripeConfig(
-    @Value("\${stripe.secret-key}")
-    private val secretKey: String
-) {
-    @PostConstruct
-    fun init() {
-        Stripe.apiKey = secretKey
+@ConfigurationProperties(prefix = "stripe")
+class StripeProperties {
+    lateinit var secretKey: String
+    lateinit var publicKey: String
+}
+
+
+@Configuration
+class StripeConfig(private val stripeProperties: StripeProperties) {
+
+    @Bean
+    fun stripeClient(): String? {
+        // Configura la clave secreta para Stripe
+        Stripe.apiKey = stripeProperties.secretKey
+        return Stripe.apiKey
     }
 }
