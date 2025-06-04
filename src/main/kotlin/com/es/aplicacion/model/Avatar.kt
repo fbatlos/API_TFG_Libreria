b0a43261da796660a4c17ec1179bbca4bf434b24
fbatlos <bdlsfrancisco@iesrafaelalberti.es>
2025-06-04 20:47:11 +0200

    Hacer imagenes de avatares, y pruebas unitarias.

--- src/main/kotlin/com/es/aplicacion/model/Avatar.kt
+++ src/main/kotlin/com/es/aplicacion/model/Avatar.kt
@@ -1,12 +1,15 @@
 ﻿package com.es.aplicacion.model
 
 import org.bson.codecs.pojo.annotations.BsonId
+import org.bson.types.Binary
 import org.springframework.data.mongodb.core.mapping.Document
 
 
 @Document("Avatares")
-data class Avatar (
+data class Avatar(
     @BsonId
-    val _id : String?,
-    val BitMap : String?
+    val _id: String? = null,
+    val filename: String,
+    val mimeType: String,
+    val data: Binary // Base64 string
 )
