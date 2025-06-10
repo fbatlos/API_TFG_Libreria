d542dd1c482ea9f6a6ee343b06592607396843e3
fbatlos <bdlsfrancisco@iesrafaelalberti.es>
2025-06-10 12:07:21 +0200

    añdido admin logica.

--- src/main/kotlin/com/es/aplicacion/controller/TicketsController.kt
+++ src/main/kotlin/com/es/aplicacion/controller/TicketsController.kt
@@ -6,7 +6,6 @@ import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.http.ResponseEntity
 import org.springframework.security.core.Authentication
 import org.springframework.web.bind.annotation.*
-import java.util.*
 
 @RestController
 @RequestMapping("/ticket")
